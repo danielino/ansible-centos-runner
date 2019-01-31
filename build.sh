b7384b18aa66d311a701f00c71b1461311deae01
Daniele Marcocci <daniele.marcocci@par-tec.it>
2019-01-31 16:05:50 +0100

    latest fixes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-for i in $(find . -type d -maxdepth 1) ; do
+for i in $(find . -type d -name "2.*" -maxdepth 1) ; do
 	cd $i
 	tagName=$(echo $i | sed 's^./^^g')
 	docker build -t ansible-centos-runner:$tagName . 
